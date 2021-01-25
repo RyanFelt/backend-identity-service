d064c0270491a42e406a13894988017a467d53e9
RyanFelt <ryanwfeltkamp@gmail.com>
2021-01-24 20:36:00 -0500

    removed dead code, update packages

--- test/dynamodbLocalServer.js
+++ test/dynamodbLocalServer.js
@@ -1,11 +1,5 @@
-require('dotenv').config();
-// const dynamodbLocal = require('dynamodb-localhost');
 const { createTables } = require('./dynamodbLocal');
 
-// const { IS_DYNAMODB_PORT } = process.env;
-
 (async () => {
-  // dynamodbLocal.install(() => {});
-  // dynamodbLocal.start({ port: 8000 });
   await createTables();
 })();
